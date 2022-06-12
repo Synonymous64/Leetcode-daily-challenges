946c39baba6c4504e695a7a76231c7aa27604b59
Synonymous64 <prajwalurkude007@gmail.com>
2022-06-12 20:46:14 +0530

    Day 12 June

--- LeetCode June Challenge/MaximumErasureValue.cpp
+++ LeetCode June Challenge/MaximumErasureValue.cpp
@@ -3,19 +3,21 @@ using namespace std;
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int n = nums.size(), left = 0, right = 0, maxLen = 0, currSum = 0;
+        int n = nums.size(), left = 0 , right = 0,currSum = 0, maxAns = 0;
+        int res = 0;
         unordered_set<int> mySet;
-        while(right < n){
-            while(mySet.find(nums[right]) != mySet.end())
-                currSum -= nums[left], mySet.erase(nums[left++]);
-            currSum += nums[right], mySet.insert(nums[right++]);
-            maxLen = max(maxLen, currSum);
+        while(right < n){   
+            while(mySet.find(nums[right]) != mySet.end()){
+                maxAns -= nums[left], mySet.erase(nums[left++]);
+            }
+            maxAns += nums[right], mySet.insert(nums[right++]);
+            res = max(res, maxAns);
         }
-        return maxLen;
+        return res;
     }
 };
 int main(){
-    vector<int> nums = {5,2,1,2,5,2,1,2,5};
+    vector<int> nums = {4,2,4,5,6};
     Solution S;
     cout<<S.maximumUniqueSubarray(nums)<<endl;
     // cout<<S.maximumUniqueSubarray1(nums)<<endl;
