e3426c6d1d8ea007799794f05e2ebbad839edd59
Synonymous64 <prajwalurkude007@gmail.com>
2022-06-17 17:26:36 +0530

    Day 17 June

---
+++ BinaryTreeCameras.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode (int value) : val(value){
+        left = nullptr;
+        right = nullptr;
+    }
+};
+class Solution {
+private:
+    int MONITERED = -1, HAS_CAMERA = 0, NEED_CAMERA = 1, count = 0;
+    int minCameraInfo(TreeNode *root){
+        if(!root) return MONITERED;
+        int left = minCameraInfo(root->left);
+        int right = minCameraInfo(root->right);
+        if(left == NEED_CAMERA || right == NEED_CAMERA){
+            count++;
+            return HAS_CAMERA;
+        }
+        else if(left == HAS_CAMERA || right == HAS_CAMERA) return MONITERED;
+        return NEED_CAMERA;
+    }
+public:
+    int minCameraCover(TreeNode* root) {
+        int rootminCameraInfo = minCameraInfo(root);
+        if(rootminCameraInfo == NEED_CAMERA) return count + 1;
+        return count;
+    }
+};
+int main(){
+    TreeNode *p1 = new TreeNode(0);
+    TreeNode *p2 = new TreeNode(0);
+    TreeNode *p3 = new TreeNode(0);
+    TreeNode *p4 = new TreeNode(0);
+    p1->left = p2;
+    p2->left = p3;
+    p2->right = p4;
+    Solution s;
+    cout<<s.minCameraCover(p1)<<endl;
+    return 0;
+}
