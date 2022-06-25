fcd5cbab630c8dc0e8fac84bc0184aa892cd7dda
Synonymous64 <prajwalurkude007@gmail.com>
2022-06-25 13:26:23 +0530

    day 25 June

---
+++ NondecreasingArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int pos = -1;
+        for(int i = 0; i < nums.length - 1; ++i){
+            if(nums[i] > nums[i + 1]){
+                if(pos != -1) return false;
+                pos = i;
+            }
+        }
+        return (pos == -1 || pos == 0 || pos == nums.length - 2 || nums[pos - 1] <= nums[pos + 1] || nums[pos] <= nums[pos + 2]);       
+    }
+}
+public class NondecreasingArray {
+    public static void main(String[] args) {
+        int[] nums = {4,2,3};
+        Solution s = new Solution();
+        System.out.println(s.checkPossibility(nums));
+    }
+}
