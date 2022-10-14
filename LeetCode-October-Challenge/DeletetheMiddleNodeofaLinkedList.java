53c29c8d81fe0af0209ae2a00c03c86048bea9cd
Synonymous64 <prajwalurkude007@gmail.com>
2022-10-14 11:51:15 +0530

    Day 14 October 2022

---
+++ LeetCode-October-Challenge/DeletetheMiddleNodeofaLinkedList.java
@@ -0,0 +1,25 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next == null) return null;
+        ListNode fast = head.next.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+public class DeletetheMiddleNodeofaLinkedList {
+    public static void main(String[] args) {
+        
+    }
+}
