a57e50f77338d4c56edb878263aaf40b3fbb862e
Synonymous64 <prajwalurkude007@gmail.com>
2022-10-18 20:41:40 +0530

    Day 18 October 2022

---
+++ LeetCode-October-Challenge/CountandSay.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 1) return "1";
+    
+        StringBuilder res = new StringBuilder();
+    
+        String s = countAndSay(n-1) + "0"; 
+        for(int i=0, c=1; i < s.length()-1; i++, c++){
+            if(s.charAt(i+1) != s.charAt(i)){
+                res.append(c).append(s.charAt(i));
+                c = 0;  
+            }
+        }
+    
+        return res.toString();
+    }
+}
+public class CountandSay {
+    public static void main(String[] args) {
+        int n = 4;
+        Solution s = new Solution();
+        System.out.println(s.countAndSay(n));
+    }
+}
