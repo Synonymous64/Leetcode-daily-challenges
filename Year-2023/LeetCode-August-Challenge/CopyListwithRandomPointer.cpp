55f661ac6e6d30cd778d3452995ba147b46a2d1f
Synonymous64 <prajwalurkude007@gmail.com>
2023-09-05 16:48:37 +0530

    September 5, 2023

---
+++ Year-2023/LeetCode-August-Challenge/CopyListwithRandomPointer.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node *, Node *> mp;
+        Node *curr = head;
+
+        while(curr){
+            Node *copy = new Node(curr->val);
+            mp[curr] = copy;
+            curr = curr->next;
+        }
+
+        curr = head;
+        while(curr){
+            mp[curr]->next = mp[curr->next];
+            mp[curr]->random = mp[curr->random];
+            curr = curr->next;
+        }
+
+        return mp[head];
+    }
+};
---
+++ Year-2023/LeetCode-August-Challenge/CopyListwithRandomPointer.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+    public Node(int val, Node _next, Node _random) {
+        this.val = val;
+        this.next = _next;
+        this.random = _random;
+    }
+}
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null)
+            return null;
+        Node cur = head;
+        while (cur != null) {
+            Node next = cur.next;
+            cur.next = new Node(cur.val, next, null);
+            cur = next;
+        }
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null)
+                cur.next.random = cur.random.next;
+            cur = cur.next.next;
+        }
+        cur = head;
+        Node copyHead = head.next;
+        while (cur != null) {
+            Node next = cur.next.next;
+            Node copy = cur.next;
+            cur.next = next;
+            if (next != null)
+                copy.next = next.next;
+            cur = next;
+        }
+        return copyHead;
+    }
+}
+
+public class CopyListwithRandomPointer {
+    public static void main(String[] args) {
+
+    }
+}
